743d366d82c97755e9bb9a923a7404ab5339ea75
JackBowln <diasv3861@gmail.com>
2022-04-01 11:31:32 -0300

    internacionalization

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <AppProvider>
-    <router-view></router-view>
+    <Internacionalization>
+      <router-view></router-view>
+    </Internacionalization>
   </AppProvider>
 </template>
 <script lang="ts">
-  import { defineComponent } from "vue"
-  import AppProvider from "./components/AppProvider.vue"
+import { defineComponent } from "vue";
+import AppProvider from "./Providers/AppProvider.vue";
+import Internacionalization from "./Providers/Internacionalization.vue";
 
-  export default defineComponent({
-    name: "App",
-    components: {
-      AppProvider,
-    },
-  })
+export default defineComponent({
+  name: "App",
+  components: {
+    AppProvider,
+  },
+});
 </script>
 <style></style>
---
+++ src/Providers/Internacionalization.vue
@@ -0,0 +1,32 @@
+  <template>
+  <n-config-provider :locale="locale" :date-locale="dateEnUS">
+    <slot />
+  </n-config-provider>
+</template>
+
+<script>
+  import { defineComponent } from 'vue'
+  import { NConfigProvider, createLocale, enUS } from 'naive-ui'
+  import { enUS, dateEnUS } from 'naive-ui'
+
+  const customizedLocale = createLocale(
+    {
+      Input: {
+        placeholder: '不提申请不构成加班'
+      }
+    },
+    enUS
+  )
+
+  export default defineComponent({
+    components: {
+      NConfigProvider
+    },
+    setup() {
+      return {
+        locale: customizedLocale,
+        dateEnUS
+      }
+    }
+  })
+</script>
